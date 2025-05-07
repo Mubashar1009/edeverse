d6de793e91625ca6a28c745f45039e435358b8b9
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-05-07 18:34:36 +0500

    fixed:build issue

--- anchor_program/tests/anchor_program.ts
+++ anchor_program/tests/anchor_program.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { AnchorProgram } from "../target/types/anchor_program";
 import { describe } from "mocha";
+import { AnchorProgram } from "../target/types/anchor_program";
 
 describe("anchor_program", () => {
   // Configure the client to use the local cluster.
