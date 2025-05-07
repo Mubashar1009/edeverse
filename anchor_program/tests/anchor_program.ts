154f981c4fdb7217811e2b1219a8492e2fd0fd29
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-05-07 19:46:05 +0500

    fix:updated type issue

--- anchor_program/tests/anchor_program.ts
+++ anchor_program/tests/anchor_program.ts
@@ -1,13 +1,11 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
 import { describe } from "mocha";
-import { AnchorProgram } from "../target/types/anchor_program";
 
 describe("anchor_program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.anchor_program as Program<AnchorProgram>;
+  const program = anchor.workspace.anchor_program;
 
   it("Is initialized!", async () => {
     // Add your test here.
