291bc5eebc31c18378bea474f71594620d7195d8
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-05-07 18:28:18 +0500

    fix:updated  types in the deploy file

--- anchor_program/migrations/deploy.ts
+++ anchor_program/migrations/deploy.ts
@@ -1,12 +1,9 @@
-import type { AnchorProvider } from "@coral-xyz/anchor";
+
 import { setProvider } from "@coral-xyz/anchor";
+import type { AnchorProvider } from "@coral-xyz/anchor";
 
-module.exports = async function (provider: AnchorProvider) {
-  setProvider(provider);
-};
-module.exports = async function (provider: AnchorProvider) {
-  // Configure client to use the provider.
-  setProvider(provider);
 
-  // Add your deploy script here.
-};
+export default async function (provider: AnchorProvider) {
+  setProvider(provider);
+}
+
